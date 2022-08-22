493d19be7741d75d87131629baced1b8eaf82944
Anthony Beckford <abeckford532@gmail.com>
2022-08-22 16:37:26 -0400

    Add files via upload

---
+++ Chapter 1 - Getting Started/MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+str += "!"
+
+// Print the string to the console
+print(str)
+
+
+// Chapter 1 Challenge
+
+var lastName = "Beckford"
+print(lastName)
