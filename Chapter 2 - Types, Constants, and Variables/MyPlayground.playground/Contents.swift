99bbe450f1ee523cfdafaf8e303124c52ed21875
Anthony Beckford <abeckford532@gmail.com>
2022-08-24 21:16:36 -0400

    Add files via upload

---
+++ Chapter 2 - Types, Constants, and Variables/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5000
+let townName: String = "Knowhere"
+
+// Bronze Challenge
+
+var townElevation: Int = 39
+
+
+var townDescription = "\(townName) has a population of \(population), has a town \(townElevation) and \(numberOfStoplights) stoplights."
+
+print(townDescription)
+
+
+
