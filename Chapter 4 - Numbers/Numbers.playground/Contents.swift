9d4ef83543d7ec317a335fe3a5961a590b82aa31
Anthony Beckford <abeckford532@gmail.com>
2022-08-31 21:15:39 -0400

    Add files via upload

---
+++ Chapter 4 - Numbers/Numbers.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//print("The maximum Int value is \(Int.max)")
+//print("The maximum Int value is \(Int.min)")
+//print("The maximun value for a 32-bit integer is \(Int32.max)")
+//print("The maximun value for a 32-bit integer is \(Int32.min)")
+//
+//print("The maximum UINt value is \(UInt.max)")
+//print("The maximum UINt value is \(UInt.min)")
+//
+//print("The maximum value for a 32-bit unsigned integer is \(UInt32.max).")
+//print("The maximum value for a 32-bit unsigned integer is \(UInt32.min).")
+
+let numberOfPages: Int = 10
+let numberOfChapters: Int = 3
+
+let numberOfPeople: UInt = 40
+let volumeAdjustment: Int32 = -1000
+
+
