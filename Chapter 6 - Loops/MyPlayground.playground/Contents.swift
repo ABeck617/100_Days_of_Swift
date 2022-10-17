d6846aff4e2d06a52c2e842701f830b86c60cfbb
Anthony Beckford <abeckford532@gmail.com>
2022-10-16 20:07:27 -0400

    Update Contents.swift

--- Chapter 6 - Loops/MyPlayground.playground/Contents.swift
+++ Chapter 6 - Loops/MyPlayground.playground/Contents.swift
@@ -1,21 +1,11 @@
 import UIKit
 
-var myFirstInt: Int = 8
-
-for i in 1...5 {
-    myFirstInt += 1
-    print("myFirstInt = \(myFirstInt) at iteration \(i)")
-}
-
 var myAge: Int = 0
 
 for i in 1...33{
     print(myAge)
     myAge += 1
 }
-
-
-
 
 // For loop with a where clause
 
@@ -24,11 +14,34 @@ for i in 1...100 where i % 3 == 0 {
 }
 
 
-// while loop
+//// while loop
+var myFirstInt: Int = 0
 
-var i = 1
-while i < 6 {
+for i in 1...5{
     myFirstInt += 1
-    print(myFirstInt)
-    i += 1
+    print("myFirstInt = \(myFirstInt) at iteration \(i)")
 }
+
+var shields = 0
+
+// repeat
+repeat {
+    print("Fire Blaster!")
+} while shields > 0
+
+
+// Fizz Buzz
+let num: Int = 100
+
+for i in 1..<num {
+    if i % 3 == 0 {
+        print("FIZZ ")
+    } else if i % 5 == 0 {
+        print("BUZZ")
+    } else if i % 3 == 0 && i % 5 == 0 {
+        print("FIZZBUZZ")
+    } else {
+        print(i)
+    }
+}
+
