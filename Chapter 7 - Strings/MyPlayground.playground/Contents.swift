147ae089876ae199ab0733edb1247fb7cbeb3d13
Anthony Beckford <abeckford532@gmail.com>
2022-10-20 14:36:24 -0400

    Add files via upload

---
+++ Chapter 7 - Strings/MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let playground = "Hello, playground"
+
+var mutablePlayground = "Hello, mutable playground"
+mutablePlayground += "!"
+let quote = "I want to \"say\":\n\(playground)"
